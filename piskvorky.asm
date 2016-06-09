53aa2295055199b224021c67fdd721bbc68bd5b4
DW0RKiN <ogion@centrum.cz>
2016-06-09 14:23:23 +0200

    Update piskvorky.asm

--- piskvorky.asm
+++ piskvorky.asm
@@ -1,54 +1,53 @@
 ;  pasmo -d piskvorky.asm piskvorky.bin > test.asm ; ./bin2tap piskvorky.bin; ls -l piskvorky.bin
  
-progStart 		equ	$C400		 ; 50176
-org progStart
+PROGSTART 		EQU	$C400		 ; 50176
+ORG PROGSTART
 
 
-PLAYER_COLOR	equ	1*8
-AI_COLOR		equ	2*8
-MASK_COLOR		equ	$38
+PLAYER_COLOR	EQU	1*8
+AI_COLOR		EQU	2*8
+MASK_COLOR		EQU	$38
 
 ; Start game from basic
-; RANDOMIZE USR 50377 (0xC4C9) = progStart + 201 (+ $00C9)
+; RANDOMIZE USR 50369 (0xC4C1) = progStart + 193 (+ $00C1)
 
 ; ----------------------------------------------------
-; Vstup: 	HL XY souradnice
+; Vstup: 	HL adresa kurzoru
 ;		A priznaky posunu
-; Vystup:	pokud neni stisknuto FIRE tak zmeni jen registry A,HL
+; Vystup:	pokud neni stisknuto FIRE tak zmeni jen registry AF,HL
 
 ;	4	3	2	1	0
 ;	F(5)	↑(4)	↓(3)	→(2)	←(1)	Sinclair 1 (Sinclair left)
-Pohyb:
-; p_left:
-	push	bc
-	push	de
+MOVE:
+; P_LEFT:
+	PUSH	BC
+	PUSH	DE
 	
-	ld	bc,$001f
-	rrca					; nastavi carry
-	ld	d,a				; maska
+	LD	BC,$001F
+	RRCA					; nastavi carry
+	LD	D,A				; maska
 
-	ld	a,l
-	sbc	a,b
-; p_right:
-	srl	d				; nastavi carry
-	adc	a,b
+	LD	A,L
+	SBC	A,B
+; P_RIGHT:
+	SRL	D				; nastavi carry
+	ADC	A,B
 	
-	and	c				; orezani do 0..31 pokud pretekl/podtekl
-	xor	l
-	and	c
-	xor	l
-	ld	l,a
+	AND	C				; orezani do 0..31 pokud pretekl/podtekl
+	XOR	L
+	AND	C
+	XOR	L
+	LD	L,A
 
-; p_down:
-	srl	d
-	jr	nc,p_up
-	adc	hl,bc
-p_up:
-	srl	d
-	jr	nc,p_fire
-	sbc	hl,bc
-p_fire:
-
+; P_DOWN:
+	SRL	D
+	JR	nc,P_UP
+	ADC	HL,BC
+P_UP:
+	SRL	D
+	JR	nc,P_FIRE
+	SBC	HL,BC
+P_FIRE:
 
 ; 57 0101 0111 +3 -> 5A 0101 1010
 ; 58 0101 1000
@@ -56,286 +55,239 @@ p_fire:
 ; 5A 0101 1010
 ; 5B 0101 1011 -3 -> 58 0101 1000
 
-IF 0
-	ld	a,h
-	cp	$58
-	sbc	a,b				; 57 -> 56 0110
-	cp	$5B
-	ccf
-	adc	a,b				; 5B -> 5C 1100
-	and	$03
-	or	$58
-	ld	h,a
-ENDIF
+	LD	A,H
+	CP	$57
+	JR	nz,P_NO_UNDERFLOW		; nepodtekl
+	LD	H,$5A				; Y max
+P_NO_UNDERFLOW:
+	CP	$5B
+	JR	nz,P_NO_OVERFLOW		; nepretekl
+	LD	H,$58				; Y min
+P_NO_OVERFLOW:
 
-	ld	a,h
-	cp	$57
-	jr	nz,p_nepodtekl
-	ld	h,$5A				; Y max
-p_nepodtekl:
-	cp	$5B
-	jr	nz,p_nepretekl
-	ld	h,$58				; Y min
-p_nepretekl:
+	SRL	D
 	
+	POP	DE
+	POP	BC
+	RET	nc				; pokud neni stisknuto FIRE tak zmeni jen registry A,HL
 
-	srl	d
-	
-	pop	de
-	pop	bc
-	ret	nc				; pokud neni stisknuto FIRE tak zmeni jen registry A,HL
-
-	ld	a,(hl)
-	or	a
-	ret	nz				; neprepisujeme kameny
-	ld	(hl),PLAYER_COLOR		; PLAYER_COLOR = 2
+	LD	A,(HL)
+	OR	A
+	RET	nz				; neprepisujeme kameny
+	LD	(HL),PLAYER_COLOR		; PLAYER_COLOR = 2
 
 ; Zjednodusene nalezeni nejlepsiho tahu
 ; Find_best:
-	ld	hl,$5800
-	ld	b,l				; = 0
-	ld	c,l				; Existuje_rada_5_kamenu = False
-	push	hl				; nejlepsi pozice
+	LD	HL,$5800
+	LD	C,L				; = 0 hodnota nejlepsi pozice, $FF = konec hry, existuje rada 5 kamenu
+	PUSH	HL				; souradnice nejlepsi pozice
 
-brain_loop:
-	
+BRAIN_LOOP:
+
+
 ; ------------------------------------
 ; Vystup:	ixh
 ; Zmeni:	IX, AF, DE
-; Count_value:	
-	ld	ixh,c				; vynulujeme hodnotu pozice
-
-	ld	de,data_priznak_smeru
-cv_loop:
-	ld	a,(de)
-	ld	(el_selfmodifying+1),a
-	inc	de
-	ld	a,(de)
-	inc	de
+; COUNT_VALUE:	
+	LD	IXH,0				; vynulujeme hodnotu aktualni pozice
+	LD	DE,DATA_DIRECTION-1
+CV_LOOP:
+	INC	DE
 	
-	push	hl
-	exx
-	pop	hl				; stinovy hl = normalni hl
+; "zpetny chod" na okraj usecky v jejimz stredu je testovana pozice
+	PUSH	HL				; na zasobnik adresu zkoumane pozice, protoze ji budeme menit jak prochazime usecky
+	LD	B,5
+EL_TO_EDGE:
+	LD	A,(DE)
+	CALL	MOVE				;
+	DJNZ	EL_TO_EDGE
+	
+	INC	DE
+; prepnuti do stinovych registru
+	EXX
 
 ; ------------------------------------
-; Vstup:	HL = XY
-; 		A opacny smer
-Explore_line:
-; "zpetny chod"
-	ld	b,5
-	ld	c,a
-el_na_okraj:
-	ld	a,c				; nastaveni smeru
-	call	Pohyb				;
-	djnz	el_na_okraj
+; Vstup:	shadow nothing, HL = cursor address, IYH value address, (DE) direction, C best value, HL on stack
+; Zmeni:	IX, AF, maybe C = $ff
+; EXPLORE_LINE:
 	
-
-; odted pujdeme na druhou stranu a stale v jednom smeru
-
 ; inicializace smycky
-	push	hl
+		LD	BC,$0900		; B = citac, C = nalezeny kamen
+		LD	HL,$0010		; hl hodnota souvisle rady, dame bonus pro ; dame bonus pro xxxx_ xxx._ xx.._ x..._
+		LD	D,C			; = 0 hodnota linie
+		LD	E,C			; = 0 prazdnych
+		LD	IXL,C			; = 0 delka rady
 
-	ld	hl,$0010			; hl hodnota souvisle rady, dame bonus pro ; dame bonus pro xxxx_ xxx._ xx.._ x..._
-	ld	d,h				; = 0 hodnota linie
-	ld	e,h				; = 0 prazdnych
-	ld	ixl,b				; = 0 delka rady
-	ld	c,b				; = 0 nalezeny kamen
-	ld	b,$09				; citac
+EL_LOOP:
+		INC	IXL			; zvedneme delku rady
 
-IF 0
-	; inicializace podvrzenim el_ruzne
-	ld	a,b				; musi byt nula a nesmi byt nula! ( jakoby hodnota z fce next_step )
-	ld	ixl,b				; = delka rady ( cokoliv mensi jak 6 )
-	ld	b,$09				; citac a hodnota dalsiho kamene
-	ld	c,b				; = nalezeny kamen ( zde cokoliv nenuloveho )
-	ld	e,$FF
-; 	nastavi podvrzeni
-; 	ld	hl,$0010			; hl hodnota souvisle rady, dame bonus pro ; dame bonus pro xxxx_ xxx._ xx.._ x..._
-; 	ld	d,h				; = 0 hodnota linie
-; 	ld	e,h				; = 0 prazdnych	
-ENDIF
-	
-	
-el_loop:
-	inc	ixl				; zvedneme delku rady
-
-el_selfmodifying:
-	ld	a,0				; nastaveni smeru
-	ex	(sp),hl
-	call	Pohyb		
-	ld	a,(hl)			; 
-	and	MASK_COLOR			; chceme jen barvu PAPER
-	ex	(sp),hl
-	jr	nz,el_nasel_kamen
+	EXX
+	; navrat do "normalnich" registru
+	LD	A,(DE)			; nastaveni smeru
+	CALL	MOVE		
+	LD	A,(HL)			; 
+	AND	MASK_COLOR			; chceme jen barvu PAPER
+	EXX
+		JR	nz,EL_FOUND_STONE
 
 ; ---------------
-; el_prazdne:
+; EL_EMPTY:
 	
-	inc	e				; prodlouzime radu prazdnych
-	add	hl,hl				; bonus za stred
+		INC	E			; prodlouzime radu prazdnych
+		ADD	HL,HL			; bonus za stred
 
-	ld	a,b				; 
-	cp	5
-	jr	z,el_next			;
+		LD	A,B			; 
+		CP	5
+		JR	z,EL_NEXT		;
 
-	add	hl,de				; H = H + D, v L a E (pocet prazdnych) je "smeti" jehoz soucet nikdy nepretece pres bajt. Pokud se sejdou jednickove bity tak je L nizke. 
-	ld	d,h
+		ADD	HL,DE			; H = H + D, v L a E (pocet prazdnych) je "smeti" jehoz soucet nikdy nepretece pres bajt. Pokud se sejdou jednickove bity tak je L nizke. 
+		LD	D,H
 	
-	ld	hl,$0010			; dostane bonus i pro priste
-	jr	el_next	
+		LD	HL,$0010		; dostane bonus i pro priste
+		JR	EL_NEXT	
 
 ; ---------------
-el_nasel_kamen:				; A je nenulove
-	cp	c				; shodne kameny?
-	jr	z,el_shodne
+EL_FOUND_STONE:				; A je nenulove
+		CP	C			; shodne kameny?
+		JR	z,EL_IDENTIC
 
-	inc	c
-	dec	c				; pokud tam byla 0 mame prvni kamen
-	ld	c,a				; ulozime novy kamen
-	jr	z,el_shodne			; prvni kamen, jinak by to blblo pri rade ._x. udelalo by to .x.
-
-; ---------------
-el_ruzne:
-	call	pricti_hodnotu_rady
-	
-	ld	hl,$0008
-	ld	d,h
-
-	inc	e				; 
-	ld 	ixl,e				; delka nove rady = prazdnych + 1 kamen
-	dec	e
-	jr	z,el_shodne
-	
-	add	hl,hl				; dame bonus 2x protoze zacal prazdnym
+		INC	C
+		DEC	C			; pokud tam byla 0 mame prvni kamen
+		LD	C,A			; ulozime novy kamen
+		JR	z,EL_IDENTIC	; prvni kamen, jinak by to blblo pri rade ._x. udelalo by to .x.
 
 ; ---------------
-el_shodne:
-
-	ld	e,0				; vymazeme radu prazdnych
-	add	hl,hl
-	add	hl,hl
+; EL_DIFFERENT:
+		CALL	ADD_VALUE_SERIES
 	
-	ld	a,h
-	cp	$20
-	jr	c,el_next
+		LD	HL,$0008
+		LD	D,H
+
+		INC	E			; 
+		LD 	IXL,E			; delka nove rady = prazdnych + 1 kamen
+		DEC	E
+		JR	z,EL_IDENTIC
+	
+		ADD	HL,HL			; dame bonus 2x protoze zacal prazdnym
+
+; ---------------
+EL_IDENTIC:
+
+		LD	E,0			; vymazeme radu prazdnych
+		ADD	HL,HL
+		ADD	HL,HL
+	
+		LD	A,H
+		CP	$20
+		JR	c,EL_NEXT
 	
 ; Nalezena rada 5 kamenu (asi)
-	exx
-	ld	a,(hl)
-	or	a				;
-	jr	z,el_prazny
-	
-	set	6,(hl)			; zesvetlime kamen
-	ld	c,$01				; Existuje_rada_5_kamenu = True
-el_prazny:
-	exx
+		EX	(SP),HL		; do HL adresa stredu zkoumane usecky
+		LD	A,(HL)
+		OR	A			;
+		JR	z,EL_5_NOT_FOUND
+; Fakt nalezena
+		SET	6,(HL)		; zesvetlime kamen
+	EXX
+	LD	C,$FF				; Existuje_rada_5_kamenu = True
+	EXX
+EL_5_NOT_FOUND:
+		EX	(SP),HL		; vratime jak to bylo
 
-el_next:
-	djnz	el_loop
+EL_NEXT:
+		DJNZ	EL_LOOP
 	
-	add	hl,hl				; dame bonus pro _xxxx _.xxx _..xx _...x
-	call	pricti_hodnotu_rady
+		ADD	HL,HL			; dame bonus pro _xxxx _.xxx _..xx _...x
+		CALL	ADD_VALUE_SERIES
 	
-	pop	hl
-
 ; end Explore_line --------------------------------------------
-	exx
+	EXX
 	
-	ld	a,data_end mod 256
-	cp	e
-	jr	nz,cv_loop
+	POP	HL				; obnovime pozici pred zkoumanim dalsiho smeru
+	LD	A,(DE)			; posledni je 1
+	DEC	A
+	JR	nz,CV_LOOP
 
 ; end Count_value ----------------------------------------------
 
-	ld	a,(hl)
-	or	a
-	jr	nz,b_next			; pokud je na pozici kamen tak uz vse ignorujeme a deme dal, test na existenci rady 5 kamenu uz probehl
+	LD	A,(HL)
+	OR	A
+	JR	nz,B_NEXT			; pokud je na pozici kamen tak uz vse ignorujeme a deme dal, test na existenci rady 5 kamenu uz probehl
 
-	ld	a,ixh
-	cp	b
-	jr	c,b_next			; pokud zname lepsi tak ignorujeme
+	LD	A,IXH
+	CP	C				; porovname s nejlepsim
+	JR	c,B_NEXT			; pokud zname lepsi tak ignorujeme
 
 ; aktualne nejlepsi pozice
-	pop	de				; vytahneme nejlepsi a zahodime
-	push	hl				; ulozime lepsi
-	ld	b,a
-b_next:
-	inc	hl
-	ld	a,$5B				; $5800 + 3 * 256 = $5800 + $0300
-	cp	h
-	jp	nz,brain_loop
+	POP	DE				; vytahneme nejlepsi a zahodime
+	PUSH	HL				; ulozime lepsi
+	LD	C,A				; nejlepsi hodnota je ulozena v C
+B_NEXT:
+	INC	HL
+	LD	A,$5B				; $5800 + 3 * 256 = $5800 + $0300
+	CP	H
+	JP	nz,BRAIN_LOOP
 
-; C = 0, 1
-	dec	c				; Existuje_rada_5_kamenu == True?
+	INC	C				; Existuje rada 5 kamenu == $FF -> $00
 	
-	pop	hl				; vytahneme nejlepsi ze zasobniku
-	ld	(hl), AI_COLOR		;
+	POP	HL				; vytahneme nejlepsi ze zasobniku
+	LD	(HL), AI_COLOR		;
 
-	ret	nz				; C = $00, $ff
-	ld	(hl),c			; = 0, zmensime pravdepodobnost ze 1/50 vteriny bude videt pixel navic nez smazem obrazovku
+	RET	nz				;
+	LD	(HL),C			; = 0, zmensime pravdepodobnost ze 1/50 vteriny bude videt pixel navic nez smazem obrazovku
+						; neni to korektni protoze teoreticky muzeme mit v HL uz obsazenou pozici pokud je $5800 uz rada peti
 ; propadnuti na Repeat_game
 
 
 ; --------------------------------
-Repeat_game:
-	pop	hl				; vytahneme nepouzitou adresu navratu pro ret
+REPEAT_GAME:
+	POP	HL				; vytahneme nepouzitou adresu navratu pro ret
 
-New_game:
+NEW_GAME:
 ; clear screen
-	ld	hl,$4000			; 3
-	ld	a,$5B				; 2
-clear_loop:
-	ld	(hl),$00			; 2
-	inc	hl				; 1
-	cp	h				; 1
-	jr	nz,clear_loop		; 2
-
-IF 0
-; clear screen
-	ld	bc,192*32+3*256-1		; 3
-	ld	de,$4001			; 3
-	ld	hl,$4000			; 3
-	ld	(hl),l			; 1 = 0
-	ldir					; 2
-						; BC = 0
-ENDIF
+	LD	HL,$4000			; 3
+	LD	A,$5B				; 2
+CLEAR_LOOP:
+	LD	(HL),$00			; 2
+	INC	HL				; 1
+	CP	H				; 1
+	JR	nz,CLEAR_LOOP		; 2
 
 ; umistime na stred a polozime kamen AI
-	ld	hl,$598F
-	ld	(hl),AI_COLOR		
+	LD	HL,$598F
+	LD	(HL),AI_COLOR		
 					
-Cti_vstup:
-	ld	e,(hl)
-	ld	(hl),$B8			; 
+READ_INPUT:
+	LD	E,(HL)
+	LD	(HL),$B8			; 
 
-	ld	bc,0xf7fe			;
-	in	a,(c)				;
-	cpl
-	cp	d
-	ld	d,a
+	LD	BC,0XF7FE			;
+	IN	A,(C)				;
+	CPL
+	CP	D
+	LD	D,A
 	
-	ld	(hl),e			; vratim puvodni
+	LD	(HL),E			; vratim puvodni
 	
-	call	nz,Pohyb	
-	jr	Cti_vstup
+	CALL	nz,MOVE	
+	JR	READ_INPUT
 
 
 ; -------------------------------
-pricti_hodnotu_rady:
+ADD_VALUE_SERIES:
 
-	ld	a,ixl				; delka rady
-	cp	$06				; je tam pricten uz i odlisny kamen
-	ret	c				; pokud ma rada i s mezerama delku kratsi jak 5 tak nema zadnou hodnotu
+		LD	A,IXL			; delka rady
+		CP	$06			; je tam pricten uz i odlisny kamen
+		RET	c			; pokud ma rada i s mezerama delku kratsi jak 5 tak nema zadnou hodnotu
 
-	add	hl,de				; H = H + D, v L a E (pocet prazdnych) je "smeti" jehoz soucet nikdy nepretece pres bajt. Pokud se sejdou jednickove bity tak je L nizke. 
-	ld	d,h
-	add	ix,de				; IXH = IXH + D, IXL si zaneradime souctem puvodni delky rady s poctem jeho prazdnych poli, ale bude se menit
+		ADD	HL,DE			; H = H + D, v L a E (pocet prazdnych) je "smeti" jehoz soucet nikdy nepretece pres bajt. Pokud se sejdou jednickove bity tak je L nizke. 
+		LD	D,H
+		ADD	IX,DE			; IXH = IXH + D, IXL si zaneradime souctem puvodni delky rady s poctem jeho prazdnych poli, ale bude se menit
 
-	ret
+		RET
 
 
 ;	4	3	2	1	0
 ;	F(5)	↑(4)	↓(3)	→(2)	←(1)	Sinclair 1 (Sinclair left)
-data_priznak_smeru:
-db	1,2,4,8,5,10,6,9			; 1&2,4&8,5&10,9&6
-data_end:
+DATA_DIRECTION:
+DB	9,6,10,5,8,4,2,1			; posledni je cislo 1! Pouzito zaroven jako zarazka
